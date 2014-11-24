9b197068cdfdbcc32dcb94a0e294c48018e1b15b
Stephanie King <stepking@amazon.com>
2014-11-24 15:00:33 -0800

    fixed include syntax

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -8,7 +8,7 @@ default_rails_layer = "rails-app"
 # If an instance is part of a custom rails layer, trick OW into thinking it's part of the main rails layer as well.
 for layer in custom_rails_layers
   if node[:opsworks][:instance][:layers].include?(layer)
-    if not node[:opsworks][:instance][:layers].include(default_rails_layer)
+    if not node[:opsworks][:instance][:layers].include?(default_rails_layer)
       normal[:opsworks][:instance][:layers] = default_rails_layer
     end
   end
