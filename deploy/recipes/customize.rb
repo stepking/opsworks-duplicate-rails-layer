aff90b74278a357e2babe26d87b44ad8849365c6
Stephanie King <stepking@amazon.com>
2014-11-24 12:29:47 -0800

    first commit

---
+++ deploy/recipes/customize.rb
@@ -0,0 +1,11 @@
+custom_rails_layers = ["railsapp1"]
+default_rails_layer = "rails-app"
+
+# If an instance is part of a custom rails layer, trick OW into thinking it's part of the main rails layer as well.
+for layer in custom_rails_layers
+  if node[:opsworks][:instance][:layers].include?(layer)
+    if not node[:opsworks][:instance][:layers].include(default_rails_layer)
+      normal[:opsworks][:instance][:layers].push(default_rails_layer)
+    end
+  end
+end
